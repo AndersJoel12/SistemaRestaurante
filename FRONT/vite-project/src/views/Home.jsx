607e506dc870e2596d1a52bcc24c035e6c57f709
AndersJoel12 <anderabreu1@gmail.com>
2025-09-28 19:13:12 -0400

    primeros pasos

---
+++ FRONT/vite-project/src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../views/ViewStyles.css'
+
+function Home(){
+    return(
+    <div style={{ background: 'lightblue' }}>
+        <h1>Hola amigo</h1>
+        <p>como estas</p>
+    </div>
+    )
+}
+
+export default Home
