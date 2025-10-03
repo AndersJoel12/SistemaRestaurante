0b3e24855b36a52cbffa742483a3dac34232d391
AndersJoel12 <anderabreu1@gmail.com>
2025-10-02 21:39:13 -0400

    agregado router e inicio de kitchen

---
+++ FRONT/vite-project/src/routes/Router.jsx
@@ -0,0 +1,15 @@
+import {Routes, Route } from 'react-router-dom';
+import Home from '../views/Home';
+/* import Menu from '../views/Menu' */
+
+function AppRouter() {
+    return(
+        <Routes>
+            <Route path='/' element={<Home />} />
+ {/*            <Route path='/menu' element={<Menu />} /> */}
+            {/* <Route path='/orders' element={< orders/>} /> */}
+        </Routes>
+    )
+}
+
+export default AppRouter
