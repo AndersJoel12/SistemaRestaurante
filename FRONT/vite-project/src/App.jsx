5b0d57f634ba6d1b2475ee5814fca12ddfbfc287
AndersJoel12 <anderabreu1@gmail.com>
2025-10-02 23:33:07 -0400

    correciones menores

--- FRONT/vite-project/src/App.jsx
+++ FRONT/vite-project/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import NavBar from "./components/Navigation.jsx";
 import Router from "./routes/Router.jsx";
-import Menu from "./views/Menu.jsx";
 
 function App() {
   return (
     <>
-      <Menu />
       <NavBar />
       <Router />
     </>
